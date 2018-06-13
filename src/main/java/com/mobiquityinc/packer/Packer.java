b3441cf698a9924520f295692ef49fac5e2085fb
Camilo Andrés Hernández Marin <camiloahm@gmail.com>
2018-06-13 10:08:17 -0500

    OutputFormatter refactor

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -1,14 +1,10 @@
 package com.mobiquityinc.packer;
 
 import com.google.common.base.Stopwatch;
-import com.google.common.base.Strings;
 import com.mobiquityinc.packer.exception.APIException;
 import com.mobiquityinc.packer.exception.ErrorCode;
-import com.mobiquityinc.packer.model.PackageOutput;
-import com.mobiquityinc.packer.model.PackageThing;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 /**
