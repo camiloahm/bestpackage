7c2aa0260c3823674577dc2ee08e6f40b7d00d5f
Camilo Andrés Hernández Marin <camiloahm@gmail.com>
2018-06-13 10:27:11 -0500

    PackageInput equals and hascode

--- src/main/java/com/mobiquityinc/packer/model/PackageInput.java
+++ src/main/java/com/mobiquityinc/packer/model/PackageInput.java
@@ -1,6 +1,7 @@
 package com.mobiquityinc.packer.model;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
  */
 @Getter
 @Builder
+@EqualsAndHashCode
 public class PackageInput {
 
     private float maxWeight;
